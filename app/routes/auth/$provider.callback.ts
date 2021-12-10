09ea773cd2f7b13bf32bc04ea8bc5c82dff515e0
Sergio Xalambrí <hello@sergiodxa.com>
2021-12-10 17:42:09 -0500

    Make auth provider dynamic

--- app/routes/auth/$provider.callback.ts
+++ app/routes/auth/$provider.callback.ts
@@ -1,8 +1,10 @@
 import { LoaderFunction } from "remix";
+import invariant from "tiny-invariant";
 import { authenticator } from "~/services/auth.server";
 
-export let loader: LoaderFunction = async ({ request }) => {
-  return authenticator.authenticate("github", request, {
+export let loader: LoaderFunction = async ({ request, params }) => {
+  invariant(params.provider, "provider is required");
+  return authenticator.authenticate(params.provider, request, {
     successRedirect: "/",
     failureRedirect: "/login",
   });
--- app/routes/auth/$provider.ts
+++ app/routes/auth/$provider.ts
@@ -1,8 +1,10 @@
 import { ActionFunction } from "remix";
+import invariant from "tiny-invariant";
 import { authenticator } from "~/services/auth.server";
 
-export let action: ActionFunction = async ({ request }) => {
-  return authenticator.authenticate("github", request, {
+export let action: ActionFunction = async ({ request, params }) => {
+  invariant(params.provider, "provider is required");
+  return authenticator.authenticate(params.provider, request, {
     successRedirect: "/",
     failureRedirect: "/login",
   });
