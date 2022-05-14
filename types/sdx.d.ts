b374ae876adb96c2a26761834a34d36538587082
Sergio Xalambrí <hello@sergiodxa.com>
2022-05-13 19:08:13 -0500

    Create new sdx.d.ts

---
+++ types/sdx.d.ts
@@ -0,0 +1,41 @@
+import type {
+  DynamicLinksFunction,
+  ExternalScriptsFunction,
+  StructuredDataFunction,
+} from "remix-utils";
+import type { PrismaClient } from "@prisma/client";
+import type { DataFunctionArgs } from "@remix-run/node";
+import type { Logger } from "winston";
+
+declare global {
+  namespace SDX {
+    export interface Context {
+      db: PrismaClient;
+      logger: Logger;
+    }
+
+    export interface LoaderFunction {
+      (
+        args: Pick<DataFunctionArgs, "request" | "params"> & {
+          context: Context;
+        }
+      ): Promise<Response> | Response;
+    }
+
+    export interface ActionFunction {
+      (
+        args: Pick<DataFunctionArgs, "request" | "params"> & {
+          context: Context;
+        }
+      ): Promise<Response> | Response;
+    }
+
+    export type Handle<LoaderData = unknown> = {
+      i18n?: string | string[];
+      hydrate?: boolean | ((data: LoaderData) => boolean);
+      scripts?: ExternalScriptsFunction;
+      dynamicLinks?: DynamicLinksFunction<LoaderData>;
+      structuredData?: StructuredDataFunction<LoaderData>;
+    };
+  }
+}
