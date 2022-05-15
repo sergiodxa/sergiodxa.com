ff36d4d7b02696afecce36b8184b3f4f5f76ffad
Sergio Xalambrí <hello@sergiodxa.com>
2022-05-14 20:28:38 -0500

    Move login test to routes folder

--- app/routes/login.test.ts
+++ app/routes/login.test.ts
@@ -1,24 +1,19 @@
-// import { open, type App } from "test/helpers/app";
 import { test, expect } from "vitest";
-import puppeteer from "puppeteer";
 import "pptr-testing-library/extend";
-import { App, open } from "test/helpers/app";
+import { type App, start } from "test/helpers/app";
 
 let app: App;
 
 beforeAll(async () => {
-  app = await open();
+  app = await start();
 });
 
 afterAll(async () => {
-  await app.close();
+  await app.stop();
 });
 
 test("Login page should render", async () => {
-  let browser = await puppeteer.launch();
-  let page = await browser.newPage();
-  await page.goto("http://localhost:3000/login");
-  let document = await page.getDocument();
+  let document = await app.navigate("/login");
   let $h2 = await document.findByRole("heading", {
     name: "Sign in to your account",
     level: 2,
@@ -30,8 +25,8 @@ test("Login page should render", async () => {
   });
   await $button.click();
 
-  await page.waitForNavigation();
+  await app.page.waitForNavigation();
 
-  let url = page.url();
+  let url = app.page.url();
   expect(url.includes("github.com")).toBeTruthy();
 });
