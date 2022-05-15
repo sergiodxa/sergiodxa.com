8399e45d018dc940b7cbe87b82b0dab0f04745a8
Sergio Xalambrí <hello@sergiodxa.com>
2022-05-14 20:28:29 -0500

    Make e2e helper clear and build the app

--- test/helpers/app.ts
+++ test/helpers/app.ts
@@ -4,18 +4,29 @@ import { execa } from "execa";
 import puppeteer from "puppeteer";
 
 export type Process = {
-  close(): Promise<void>;
+  stop(): Promise<void>;
   port: number;
 };
 
 export type App = {
   navigate(path: string): Promise<puppeteer.ElementHandle<Element>>;
-  close(): Promise<void>;
+  stop(): Promise<void>;
   browser: puppeteer.Browser;
   page: puppeteer.Page;
 };
 
-async function process() {
+function clearBuild() {
+  return Promise.all([
+    execa("rm", ["-rf", "server/build"]),
+    execa("rm", ["-rf", "public/build"]),
+  ]);
+}
+
+function buildApp() {
+  return execa("npm", ["run", "build"]);
+}
+
+async function startProcess() {
   let port = await getPort();
 
   let server = execa("npm", ["start"], {
@@ -23,7 +34,12 @@ async function process() {
       CI: "true",
       NODE_ENV: "test",
       PORT: port.toString(),
+      BASE_URL: `http://localhost:${port}`,
     },
+  });
+
+  server.catch((error) => {
+    console.error(error);
   });
 
   return await new Promise<Process>(async (resolve, reject) => {
@@ -31,7 +47,7 @@ async function process() {
     server.stdout.on("data", (stream: Buffer) => {
       if (stream.toString().includes(port.toString())) {
         return resolve({
-          async close() {
+          async stop() {
             if (server.killed) return;
             server.cancel();
           },
@@ -42,10 +58,23 @@ async function process() {
   });
 }
 
-export async function open(): Promise<App> {
-  let { port, close } = await process();
-  let browser = await puppeteer.launch();
-  let page = await browser.newPage();
+function openBrowser() {
+  return puppeteer.launch();
+}
+
+function openPage(browser: puppeteer.Browser) {
+  return browser.newPage();
+}
+
+export async function start(): Promise<App> {
+  await clearBuild();
+  await buildApp();
+
+  let { port, stop } = await startProcess();
+
+  let browser = await openBrowser();
+  let page = await openPage(browser);
+
   return {
     browser,
     page,
@@ -54,9 +83,10 @@ export async function open(): Promise<App> {
       await page.goto(url.toString());
       return await page.getDocument();
     },
-    async close() {
-      await close();
+    async stop() {
+      await stop();
       await browser.close();
+      await clearBuild();
     },
   };
 }
