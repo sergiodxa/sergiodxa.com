1bbd849abf108d2447075346e2edf710c6d5205f
Sergio Xalambrí <hello@sergiodxa.com>
2022-06-02 15:29:39 -0500

    Update more things

--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -1,19 +1,16 @@
-import type { User } from "@prisma/client";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 import { badRequest, unauthorized } from "remix-utils";
 import { auth } from "~/services/auth.server";
 import writeAnArticle from "~/use-cases/write-an-article";
 
-type LoaderData = {
-  userId: User["id"];
-};
+type LoaderData = null;
+
+export let handle: SDX.Handle = { hydrate: true };
 
 export let loader: SDX.LoaderFunction = async ({ request }) => {
-  let userId = await auth.isAuthenticated(request, {
-    failureRedirect: "/login",
-  });
-  return json<LoaderData>({ userId });
+  await auth.isAuthenticated(request, { failureRedirect: "/login" });
+  return json<LoaderData>(null);
 };
 
 export let action: SDX.ActionFunction = async ({ request, context }) => {
@@ -21,18 +18,24 @@ export let action: SDX.ActionFunction = async ({ request, context }) => {
   if (!userId) return unauthorized({ message: "Unauthorized" });
   let formData = await request.formData();
   formData.set("authorId", userId);
-  let result = await writeAnArticle(context, formData);
+  let result = await writeAnArticle(
+    {
+      authorId: userId,
+      title: formData.get("title") as string,
+      body: formData.get("body") as string,
+    },
+    context
+  );
   if (result.status === "failure") return badRequest(result.error.message);
   return redirect("/articles");
 };
 
 export default function Write() {
-  let { userId } = useLoaderData<LoaderData>();
   let error = useActionData<string>();
+
   return (
     <Form method="post">
       {error ? <p>{error}</p> : null}
-      <input type="hidden" name="authorId" value={userId} />
       <input type="text" name="title" />
       <textarea name="body" />
       <button>Create</button>
