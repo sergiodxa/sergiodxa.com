6a5a73fc445b253c1b1d60fc0b49bd7e9245d516
Sergio Xalambrí <hello@sergiodxa.com>
2022-06-23 17:48:34 -0500

    Remove use cases code

--- framework/use-case/errors.ts
+++
@@ -1,9 +0,0 @@
-/**
- * An error thrown when the validation check of a use case fails.
- */
-export class ValidationError extends AggregateError {}
-
-/**
- * An error thrown when the execution of a use case fails.
- */
-export class BehaviorError extends AggregateError {}
