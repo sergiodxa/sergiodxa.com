ba4b905637c17542e77378f2161adfe528e218a1
Sergio Xalambrí <hello@sergiodxa.com>
2022-08-02 23:43:58 -0500

    Add /r/:redirectId route

---
+++ app/routes/r.$redirectId.ts
@@ -0,0 +1,29 @@
+import { redirect, type LoaderArgs } from "@remix-run/node";
+import invariant from "tiny-invariant";
+
+const REDIRECTS = {
+  "remix-next": "https://sergiodxa.com/articles/remix-vs-next-js-comparison",
+  "remix-auth-return-to":
+    "https://sergiodxa.com/articles/add-returnto-behavior-to-remix-auth",
+  "cache-http-server":
+    "https://sergiodxa.com/articles/http-vs-server-side-cache-in-remix",
+  "cache-loader-route":
+    "https://sergiodxa.com/articles/loader-vs-route-cache-headers-in-remix",
+};
+
+export async function loader({ params, context }: LoaderArgs) {
+  try {
+    let { redirectId } = params;
+
+    invariant(redirectId, "The redirectId is required");
+    invariant(
+      Object.keys(REDIRECTS).includes(redirectId),
+      `The redirectId "${redirectId}" is not valid`
+    );
+
+    return redirect(REDIRECTS[redirectId as keyof typeof REDIRECTS]);
+  } catch (error) {
+    context.logger.error(error);
+    return redirect("/");
+  }
+}
