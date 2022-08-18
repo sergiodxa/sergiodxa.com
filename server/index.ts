9bc2a0af5d8d561f5b6c5d99a771fbec1d2b1494
Sergio Xalambrí <hello@sergiodxa.com>
2022-08-18 04:58:39 -0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,14 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
 
-// import { envSchema } from "./env";
+import { envSchema } from "./env";
 
 const handleRequest = createPagesFunctionHandler({
 	build,
 	mode: process.env.NODE_ENV,
 	getLoadContext(context) {
-		// let env = envSchema.parse(context.env);
-		return { env: context.env };
+		let env = envSchema.parse(context.env);
+		return { env };
 	},
 });
 
