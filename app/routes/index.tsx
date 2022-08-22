b905165a67cde8816cb64828b372629032abbdcc
Sergio Xalambrí <hello@sergiodxa.com>
2022-08-22 06:03:59 -0500

    Add more UI

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,9 @@
 import type { LoaderArgs } from "@remix-run/cloudflare";
 
 import { json } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
+import { useT } from "~/helpers/use-i18n.hook";
 import { AirtableService } from "~/services/airtable.server";
 import { CollectedNotesService } from "~/services/cn.server";
 
@@ -20,25 +21,65 @@ export async function loader({ context }: LoaderArgs) {
 	);
 
 	let start = Date.now();
-	let [latestNotes, bookmarks] = await Promise.all([
+	let [notes, bookmarks] = await Promise.all([
 		cn.getLatestNotes(),
 		airtable.getBookmarks(10),
 	]);
 	let end = Date.now();
 
-	return json({ diff: end - start, latestNotes, bookmarks });
+	return json({
+		diff: end - start,
+		notes: notes.slice(0, 10),
+		bookmarks: bookmarks,
+	});
 }
 
 export let handle: SDX.Handle = { hydrate: true };
 
 export default function Index() {
-	let { latestNotes, bookmarks } = useLoaderData<typeof loader>();
+	let { notes, bookmarks } = useLoaderData<typeof loader>();
+	let t = useT();
 	return (
-		<article className="prose mx-auto my-8">
-			<h1>Home</h1>
-			<pre>
-				<code>{JSON.stringify({ latestNotes }, null, "\t")}</code>
-			</pre>
-		</article>
+		<main className="mx-auto grid max-w-screen-lg grid-cols-2 gap-8">
+			<section className="space-y-2">
+				<h2 className="text-lg font-semibold">{t("Latest notes")}</h2>
+				<p>{t("These are my latests articles")}</p>
+
+				<ul className="space-y-1">
+					{notes.map((note) => {
+						return (
+							<li key={note.id}>
+								<Link
+									to={note.path}
+									className="text-blue-600 underline visited:text-violet-600"
+								>
+									{note.title}
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</section>
+
+			<section className="space-y-2">
+				<h2 className="text-lg font-semibold">{t("Recent bookmarks")}</h2>
+				<p>{t("The latests links I have bookmarked")}</p>
+
+				<ul className="space-y-1">
+					{bookmarks.map((bookmark) => {
+						return (
+							<li key={bookmark.id}>
+								<a
+									href={bookmark.url}
+									className="text-blue-600 underline visited:text-violet-600"
+								>
+									{bookmark.title}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+			</section>
+		</main>
 	);
 }
