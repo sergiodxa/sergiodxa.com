ecc79a06ad15a7677aa5a2837e0eaf020c67af9e
Sergio Xalambrí <hello@sergiodxa.com>
2022-08-22 07:13:37 -0500

    Update Tailwind config

--- config/tailwind.js
+++ config/tailwind.js
@@ -1,10 +1,36 @@
 module.exports = {
 	content: ["./app/**/*.{ts,tsx}"],
+
 	darkMode: "media",
+
 	theme: {
-		extend: {},
+		extend: {
+			maxWidth: {
+				prose: "65ch",
+			},
+
+			inset: {
+				full: "100%",
+			},
+
+			scale: {
+				500: "5",
+			},
+
+			fontSize: {
+				"7xl": "5rem",
+				"8xl": "6rem",
+			},
+		},
 	},
-	variants: {},
+
+	variants: {
+		extend: {
+			padding: ["first", "last"],
+			typography: ["dark"],
+		},
+	},
+
 	plugins: [
 		require("@tailwindcss/typography"),
 		require("@tailwindcss/forms"),
