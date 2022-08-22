0228ef59ebdb4f8494719675fb4f8c93286ed359
Sergio Xalambrí <hello@sergiodxa.com>
2022-08-22 05:50:11 -0500

    Use new services on routes/index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,25 +3,42 @@ import type { LoaderArgs } from "@remix-run/cloudflare";
 import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 
-import { MarkdownView } from "~/components/markdown";
-import { GitHubService } from "~/services/gh.server";
-import { parseMarkdown } from "~/services/md.server";
+import { AirtableService } from "~/services/airtable.server";
+import { CollectedNotesService } from "~/services/cn.server";
 
 export async function loader({ context }: LoaderArgs) {
-	let gh = new GitHubService(context!.env.GITHUB_TOKEN);
+	let cn = new CollectedNotesService(
+		context!.env.CN_EMAIL,
+		context!.env.CN_TOKEN,
+		context!.env.CN_SITE
+	);
+
+	let airtable = new AirtableService(
+		context!.env.AIRTABLE_API_KEY,
+		context!.env.AIRTABLE_BASE,
+		context!.env.AIRTABLE_TABLE_ID
+	);
+
 	let start = Date.now();
-	let content = await gh.getArticleContent("about");
+	let [latestNotes, bookmarks] = await Promise.all([
+		cn.getLatestNotes(),
+		airtable.getBookmarks(10),
+	]);
 	let end = Date.now();
-	return json({ article: parseMarkdown(content), diff: end - start });
+
+	return json({ diff: end - start, latestNotes, bookmarks });
 }
 
 export let handle: SDX.Handle = { hydrate: true };
 
 export default function Index() {
-	let { article } = useLoaderData<typeof loader>();
+	let { latestNotes, bookmarks } = useLoaderData<typeof loader>();
 	return (
 		<article className="prose mx-auto my-8">
-			<MarkdownView content={article} />
+			<h1>Home</h1>
+			<pre>
+				<code>{JSON.stringify({ latestNotes }, null, "\t")}</code>
+			</pre>
 		</article>
 	);
 }
