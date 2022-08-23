9256da374d0a8050eba5846249c51ba5566d996e
Sergio Xalambrí <hello@sergiodxa.com>
2022-08-22 23:40:58 -0500

    Cache /rss response

--- app/routes/rss.ts
+++ app/routes/rss.ts
@@ -1,3 +1,11 @@
 export async function loader() {
-	return fetch("https://collectednotes.com/sergiodxa/feed/public_site.rss");
+	let response = await fetch(
+		"https://collectednotes.com/sergiodxa/feed/public_site.rss"
+	);
+
+	let headers = new Headers(response.headers);
+
+	headers.set("cache-control", "s-maxage=3600, stale-while-revalidate");
+
+	return new Response(response.body, { headers });
 }
