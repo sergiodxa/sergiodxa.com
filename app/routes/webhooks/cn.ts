dfd45e191123f63ee874abb409e3ea859610e61a
Sergio Xalambrí <hello@sergiodxa.com>
2022-09-09 16:09:17 -0500

    Update cn.ts

--- app/routes/webhooks/cn.ts
+++ app/routes/webhooks/cn.ts
@@ -3,4 +3,5 @@ import type { ActionArgs } from "@remix-run/cloudflare";
 export async function action({ request, context }: ActionArgs) {
 	let webhook = context.services.cn.parseWebhookBody(await request.json());
 	await context.services.cn.emit(webhook);
+	return null;
 }
