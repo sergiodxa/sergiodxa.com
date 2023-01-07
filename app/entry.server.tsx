1dceef0bd49435391d8378529c15f85a5a856052
Sergio Xalambrí <hello@sergiodxa.com>
2023-01-07 03:29:43 -0500

    Use preloadLinkedAssets from Remix Utils

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,12 +4,12 @@ import { RemixServer } from "@remix-run/react";
 import { createInstance } from "i18next";
 import { renderToString } from "react-dom/server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
+import { preloadLinkedAssets } from "remix-utils";
 
 import en from "~/locales/en";
 import es from "~/locales/es";
 import { i18n } from "~/services/i18n.server";
-
-import { measure } from "./utils/measure";
+import { measure } from "~/utils/measure";
 
 export default function handleRequest(
 	request: Request,
@@ -41,40 +41,11 @@ export default function handleRequest(
 
 		headers.set("Content-Type", "text/html");
 
-		prefetchAssets(context, headers);
+		preloadLinkedAssets(context, headers);
 
 		return new Response("<!DOCTYPE html>" + markup, {
 			status: statusCode,
 			headers: headers,
 		});
 	});
-}
-
-function prefetchAssets(context: EntryContext, headers: Headers) {
-	let links = context.matches
-		.flatMap((match) => {
-			let route = context.routeModules[match.route.id];
-			if (route.links instanceof Function) return route.links();
-			return [];
-		})
-		.map((link) => {
-			if ("as" in link && "href" in link) {
-				return { href: link.href, as: link.as } as const;
-			}
-			if ("rel" in link && "href" in link) {
-				if (link.rel === "stylesheet")
-					return { href: link.href, as: "style" } as const;
-			}
-			return null;
-		})
-		.filter((link: any): link is { href: string; as: string } => {
-			return link && "href" in link;
-		})
-		.filter((item, index, list) => {
-			return index === list.findIndex((link) => link.href === item.href);
-		});
-
-	for (let link of links) {
-		headers.append("Link", `<${link.href}>; rel=preload; as=${link.as}`);
-	}
 }
