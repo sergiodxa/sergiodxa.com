8854354016cad736f86ecf7b5541d0442b8160b9
Sergio Xalambrí <hello@sergiodxa.com>
2023-01-26 03:31:00 -0500

    Use Zod pipe to type-check Markdown attributes

--- entities/markdown.ts
+++ entities/markdown.ts
@@ -1,15 +1,17 @@
 import matter from "front-matter";
 import { z } from "zod";
 
-export const MarkdownSchema = z.string().transform((content) => {
-	let { attributes, body } = matter(content);
-
-	return {
-		attributes: z
-			.record(z.union([z.string(), z.date(), z.string().array()]))
-			.parse(attributes),
-		body,
-	};
-});
+export const MarkdownSchema = z
+	.string()
+	.transform((content) => {
+		let { attributes, body } = matter(content);
+		return { attributes, body };
+	})
+	.pipe(
+		z.object({
+			attributes: z.record(z.union([z.string(), z.date(), z.string().array()])),
+			body: z.string(),
+		})
+	);
 
 export type Markdown = z.infer<typeof MarkdownSchema>;
