3d0ddf2b166854cbfd37a211c83164fd5b9d9220
Sergio Xalambrí <hello@sergiodxa.com>
2023-04-10 10:25:24 -0500

    Replace formData schema with an updated version
    Also include tests

---
+++ app/utils/schemas.test.ts
@@ -0,0 +1,159 @@
+import { describe } from "vitest";
+import { z } from "zod";
+
+import { Schemas } from "./schemas";
+
+describe("Schemas", () => {
+	describe(Schemas.searchParams.name, () => {
+		test("fails if parsed value is not a URLSearchParams", () => {
+			expect(() => Schemas.searchParams().parse({})).toThrow();
+		});
+
+		test("parses a URLSearchParams", () => {
+			expect(Schemas.searchParams().parse(new URLSearchParams())).toEqual({});
+		});
+
+		test("parses a URLSearchParams with a single value", () => {
+			expect(
+				Schemas.searchParams().parse(new URLSearchParams("foo=bar"))
+			).toEqual({ foo: "bar" });
+		});
+
+		test("parses a URLSearchParams with multiple values", () => {
+			expect(
+				Schemas.searchParams().parse(new URLSearchParams("foo=bar&foo=baz"))
+			).toEqual({ foo: ["bar", "baz"] });
+		});
+
+		test("parses a URLSearchParams with multiple values and a single value", () => {
+			expect(
+				Schemas.searchParams().parse(
+					new URLSearchParams("foo=bar&foo=baz&bar=qux")
+				)
+			).toEqual({ foo: ["bar", "baz"], bar: "qux" });
+		});
+
+		test("coerces a URLSearchParams with a single value to a number", () => {
+			expect(
+				Schemas.searchParams()
+					.pipe(
+						z.object({
+							foo: z.coerce.number().nullable().default(1),
+						})
+					)
+					.parse(new URLSearchParams("foo=123"))
+			).toEqual({ foo: 123 });
+		});
+
+		test("coerces a URLSearchParams with multiple values to a number", () => {
+			expect(
+				Schemas.searchParams()
+					.pipe(
+						z.object({
+							foo: z.coerce.number().array(),
+						})
+					)
+					.parse(new URLSearchParams("foo=123&foo=456"))
+			).toEqual({ foo: [123, 456] });
+		});
+
+		test("coerces a URLSearchParams with a single value to a boolean", () => {
+			expect(
+				Schemas.searchParams()
+					.pipe(
+						z.object({
+							foo: z.coerce.boolean().nullable().default(true),
+						})
+					)
+					.parse(new URLSearchParams("foo=true"))
+			).toEqual({ foo: true });
+		});
+	});
+
+	describe(Schemas.formData.name, () => {
+		test("fails if parsed value is not a FormData", () => {
+			expect(() => Schemas.formData().parse({})).toThrow();
+		});
+
+		test("parses a FormData", () => {
+			expect(Schemas.formData().parse(new FormData())).toEqual({});
+		});
+
+		test("parses a FormData with a single value", () => {
+			let formData = new FormData();
+			formData.set("foo", "bar");
+
+			expect(Schemas.formData().parse(formData)).toEqual({
+				foo: "bar",
+			});
+		});
+
+		test("parses a FormData with multiple values", () => {
+			let formData = new FormData();
+			formData.append("foo", "bar");
+			formData.append("foo", "baz");
+
+			expect(Schemas.formData().parse(formData)).toEqual({
+				foo: ["bar", "baz"],
+			});
+		});
+
+		test("parses a FormData with multiple values and a single value", () => {
+			let formData = new FormData();
+			formData.append("foo", "bar");
+			formData.append("foo", "baz");
+			formData.set("bar", "qux");
+
+			expect(Schemas.formData().parse(formData)).toEqual({
+				foo: ["bar", "baz"],
+				bar: "qux",
+			});
+		});
+
+		test("coerces a FormData with a single value to a number", () => {
+			let formData = new FormData();
+			formData.set("foo", "123");
+
+			expect(
+				Schemas.formData()
+					.pipe(
+						z.object({
+							foo: z.coerce.number().nullable().default(1),
+						})
+					)
+					.parse(formData)
+			).toEqual({ foo: 123 });
+		});
+
+		test("coerces a FormData with multiple values to a number", () => {
+			let formData = new FormData();
+			formData.append("foo", "123");
+			formData.append("foo", "456");
+
+			expect(
+				Schemas.formData()
+					.pipe(
+						z.object({
+							foo: z.coerce.number().array(),
+						})
+					)
+					.parse(formData)
+			).toEqual({ foo: [123, 456] });
+		});
+
+		test("coerces a FormData with a single value to a boolean", () => {
+			let formData = new FormData();
+			formData.set("foo", "true");
+
+			expect(
+				Schemas.formData()
+					.pipe(
+						z.object({
+							foo: z.coerce.boolean().nullable().default(true),
+						})
+					)
+					.parse(formData)
+			).toEqual({ foo: true });
+		});
+	});
+});
--- app/utils/schemas.ts
+++ app/utils/schemas.ts
@@ -1,12 +1,51 @@
 import { z } from "zod";
 
-export function formData() {
-	return z.instanceof(FormData).transform((formData) => {
-		return z
-			.array(
-				z.tuple([z.string(), z.string().or(z.instanceof(File)).nullable()])
-			)
-			.transform((entries) => Object.fromEntries(entries))
-			.parse(Object.entries(formData));
-	});
+import { isEmpty } from "./arrays";
+
+export namespace Schemas {
+	export let FormDataEntryValueSchema = z
+		.union([z.string(), z.instanceof(File)])
+		.nullable();
+
+	export function formData() {
+		return z.instanceof(FormData).transform((formData) => {
+			let entries: Array<[string, FormDataEntryValue | FormDataEntryValue[]]> =
+				[];
+
+			for (let key of formData.keys()) {
+				let value = formData.getAll(key);
+				if (isEmpty(value)) continue;
+				if (value.length === 1) entries.push([key, value[0]]);
+				else entries.push([key, value]);
+			}
+
+			return z
+				.array(
+					z.tuple([
+						z.string(),
+						FormDataEntryValueSchema.or(FormDataEntryValueSchema.array()),
+					])
+				)
+				.transform((entries) => Object.fromEntries(entries))
+				.parse(entries);
+		});
+	}
+
+	export function searchParams() {
+		return z.instanceof(URLSearchParams).transform((searchParams) => {
+			let entries: Array<[string, string | string[]]> = [];
+
+			for (let key of searchParams.keys()) {
+				let value = searchParams.getAll(key);
+				if (isEmpty(value)) continue;
+				if (value.length === 1) entries.push([key, value[0]]);
+				else entries.push([key, value]);
+			}
+
+			return z
+				.array(z.tuple([z.string(), z.string().or(z.string().array())]))
+				.transform((entries) => Object.fromEntries(entries))
+				.parse(entries);
+		});
+	}
 }
