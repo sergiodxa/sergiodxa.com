fc0a44c9dfb5d737c273b944e2a9fbfd22c0672c
Sergio Xalambrí <hello@sergiodxa.com>
2023-04-30 01:43:48 -0500

    Add restore from localStorage support

--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -2,6 +2,7 @@ import type { DataFunctionArgs } from "@remix-run/cloudflare";
 
 import { json } from "@remix-run/cloudflare";
 import { useFetcher } from "@remix-run/react";
+import { useEffect, useState } from "react";
 import { z } from "zod";
 
 import { MarkdownView } from "~/components/markdown";
@@ -27,22 +28,67 @@ export async function action({ request, context }: DataFunctionArgs) {
 }
 
 export default function Component() {
-	let fetcher = useFetcher<typeof action>();
+	let { submit, data } = useFetcher<typeof action>();
+
+	let [savedContent, setSavedContent] = useState<string | null>(null);
+	let [content, setContent] = useState("");
+
+	useEffect(
+		function fetchParsedContent() {
+			submit({ content }, { method: "post" });
+		},
+		[content, submit]
+	);
+
+	useEffect(() => {
+		let saved = localStorage.getItem("write");
+		if (!saved) return;
+		setSavedContent(saved);
+	}, []);
 
 	return (
-		<main className="mx-auto grid min-h-[calc(100vh-90px-32px)] max-w-screen-xl gap-4 sm:grid-cols-2">
+		<main
+			className="mx-auto grid min-h-[calc(100vh-90px-32px)] max-w-screen-xl gap-4 sm:grid-cols-2"
+			style={{
+				gridTemplateRows: savedContent ? "auto 1fr" : "1fr",
+			}}
+		>
+			{savedContent && (
+				<aside className="col-span-2 h-6">
+					<button
+						type="button"
+						onClick={() => {
+							if (savedContent) setContent(savedContent);
+							setSavedContent(null);
+							localStorage.removeItem("write");
+						}}
+					>
+						Restore
+					</button>
+					<button
+						type="button"
+						onClick={() => {
+							setSavedContent(null);
+							localStorage.removeItem("write");
+						}}
+					>
+						Discard
+					</button>
+				</aside>
+			)}
+
 			<textarea
+				value={content}
 				onChange={(event) => {
-					let content = event.currentTarget.value;
-					fetcher.submit({ content }, { method: "post" });
+					let value = event.currentTarget.value;
+					setContent(value);
+					localStorage.setItem("write", value);
 				}}
 				className="resize-none"
 			/>
 
 			<div className="prose prose-blue max-w-prose">
-				{fetcher.data?.content ? (
-					<MarkdownView content={fetcher.data.content} />
-				) : null}
+				{data?.content ? <MarkdownView content={data.content} /> : null}
 			</div>
 		</main>
 	);
