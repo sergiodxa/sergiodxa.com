cf954d99849584d2ec1bc0beb542e371a5aadede
Sergio Xalambrí <hello@sergiodxa.com>
2023-05-02 09:52:49 -0500

    Move route to folder and use new editor

--- app/routes/write/route.tsx
+++ app/routes/write/route.tsx
@@ -2,12 +2,14 @@ import type { DataFunctionArgs } from "@remix-run/cloudflare";
 
 import { json } from "@remix-run/cloudflare";
 import { useFetcher } from "@remix-run/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { z } from "zod";
 
 import { MarkdownView } from "~/components/markdown";
 import { parseMarkdown } from "~/md.server";
 import { Schemas } from "~/utils/schemas";
+
+import { Editor } from "./editor";
 
 export let handle: SDX.Handle = { hydrate: true };
 
@@ -33,35 +35,29 @@ export default function Component() {
 	let [savedContent, setSavedContent] = useState<string | null>(null);
 	let [content, setContent] = useState("");
 
-	useEffect(
-		function fetchParsedContent() {
-			submit({ content }, { method: "post" });
-		},
-		[content, submit]
-	);
-
 	useEffect(function restoreDraftContent() {
 		let saved = localStorage.getItem("write");
-		if (!saved) return;
-		setSavedContent(saved);
+		if (saved) return setSavedContent(saved);
 	}, []);
 
-	useEffect(
-		function saveDraftContent() {
+	let handleChange = useCallback(
+		(content: string) => {
+			setContent(content);
 			localStorage.setItem("write", content);
+			submit({ content }, { method: "post" });
 		},
-		[content]
+		[submit]
 	);
 
 	return (
 		<main
 			className="mx-auto grid min-h-[calc(100vh-90px-32px)] max-w-screen-xl gap-4 sm:grid-cols-2"
 			style={{
-				gridTemplateRows: savedContent ? "auto 1fr" : "1fr",
+				gridTemplateRows: savedContent ? "auto auto 1fr" : "auto 1fr",
 			}}
 		>
 			{savedContent && (
-				<aside className="col-span-2 h-6">
+				<aside className="col-span-2">
 					<button
 						type="button"
 						onClick={() => {
@@ -83,15 +79,7 @@ export default function Component() {
 				</aside>
 			)}
 
-			<textarea
-				value={content}
-				onChange={(event) => {
-					let value = event.currentTarget.value;
-					setContent(value);
-					localStorage.setItem("write", value);
-				}}
-				className="resize-none"
-			/>
+			<Editor value={content} onChange={handleChange} />
 
 			<div className="prose prose-blue max-w-prose">
 				{data?.content ? <MarkdownView content={data.content} /> : null}
