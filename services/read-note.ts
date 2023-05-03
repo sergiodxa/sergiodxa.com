55371c316e7a7282598e86324f8ccad52803ac8b
Sergio Xalambrí <hello@sergiodxa.com>
2023-05-03 09:21:18 -0500

    Update read-note.ts

--- services/read-note.ts
+++ services/read-note.ts
@@ -51,9 +51,7 @@ export class ReadNoteService extends Service {
 		let cached = await this.kv.get(`note:${path}`, "json");
 		if (cached) return NoteSchema.parse(cached);
 
-		let note = await this.repos.cn.request("GET /:sitePath/:notePath", {
-			variables: { params: { sitePath: "sergiodxa", notePath: path! } },
-		});
+		let note = await this.repos.notes.fetchNoteByPath(path);
 
 		let result: z.infer<typeof NoteSchema> = NoteSchema.parse({
 			path,
@@ -61,8 +59,8 @@ export class ReadNoteService extends Service {
 			title: note.title,
 			headline: note.headline,
 			wordCount: note.body.split(/\s+/).length,
-			datePublished: note.createdAt,
-			dateModified: note.updatedAt,
+			datePublished: note.created_at,
+			dateModified: note.updated_at,
 		});
 
 		await this.kv.put(`note:${path}`, JSON.stringify(result), {
