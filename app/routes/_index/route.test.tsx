852704d817a4d64ff88d218b5e9da50e6f947b99
Sergio Xalambrí <hello@sergiodxa.com>
2023-05-09 02:35:49 -0500

    Add test to routes/_index loader

---
+++ app/routes/_index/route.test.tsx
@@ -0,0 +1,37 @@
+import { describe, expect } from "vitest";
+import { loader } from "./route";
+import { AppLoadContext } from "@remix-run/cloudflare";
+
+describe(loader.name, () => {
+	test("returns feed data", async () => {
+		let response = await loader({
+			request: new Request("https://sergiodxa.com/"),
+			params: {},
+			context: {
+				time<Type>(_: string, fn: () => Type) {
+					return fn();
+				},
+				services: {
+					log: {
+						http() {},
+					},
+					feed: {
+						async perform() {
+							return {
+								notes: [],
+								bookmarks: [],
+								tutorials: [],
+							};
+						},
+					},
+				},
+			} as unknown as AppLoadContext,
+		});
+
+		await expect(response.json()).resolves.toEqual({
+			notes: [],
+			bookmarks: [],
+			tutorials: [],
+		});
+	});
+});
