324cf266147ea9e2e0b0adcd7b5769123e08942c
Sergio Xalambrí <hello@sergiodxa.com>
2023-07-13 00:09:28 -0500

    Update use-i18n.hook.ts

--- app/helpers/use-i18n.hook.ts
+++ app/helpers/use-i18n.hook.ts
@@ -1,12 +1,12 @@
 import type { KeyPrefix, Namespace } from "i18next";
-import type { DefaultNamespace } from "react-i18next/TransWithoutContext";
+import type { _DefaultNamespace } from "react-i18next/TransWithoutContext";
 
 import { useTranslation } from "react-i18next";
 
 export function useT<
-	N extends Namespace = DefaultNamespace,
+	N extends Namespace = _DefaultNamespace,
 	TKPrefix extends KeyPrefix<N> = undefined
->(ns?: N | Readonly<N>, keyPrefix?: TKPrefix) {
+>(ns?: string, keyPrefix?: TKPrefix) {
 	return useTranslation(ns, { keyPrefix }).t;
 }
 
