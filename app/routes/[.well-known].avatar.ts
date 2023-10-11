91a5734a3b1d9aa18d62dccb89d06b608ab520bb
Sergio Xalambrí <hello@sergiodxa.com>
2023-10-11 18:55:35 -0500

    Add script to pass Prettier and run linter

--- app/routes/[.well-known].avatar.ts
+++ app/routes/[.well-known].avatar.ts
@@ -2,7 +2,7 @@ import type { LoaderArgs } from "@remix-run/cloudflare";
 
 import avatar from "~/assets/avatar.png";
 
-export function loader({request}: LoaderArgs) {
-  let url = new URL(avatar, request.url);
-  return fetch(url);
+export function loader({ request }: LoaderArgs) {
+	let url = new URL(avatar, request.url);
+	return fetch(url);
 }
