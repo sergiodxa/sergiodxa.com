979856eeb3ed5fc020bb39ce9cb60c5b0cae8c0b
Sergio Xalambrí <hello@sergiodxa.com>
2023-12-22 16:29:54 -0500

    Create UUID utils

---
+++ app/utils/uuid.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export type UUID = string & { __uuid: true };
+
+export function generateUUID() {
+	return crypto.randomUUID() as UUID;
+}
+
+export function assertUUID(value: unknown): asserts value is UUID {
+	if (!z.string().uuid().safeParse(value).success) {
+		throw new Error("Invalid UUID");
+	}
+}
