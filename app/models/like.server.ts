299c9931f36d3f67fb58e330ff7001fd1d32bb22
Sergio Xalambrí <hello@sergiodxa.com>
2023-12-22 16:32:22 -0500

    Update Like model to use new Post model

--- app/models/like.server.ts
+++ app/models/like.server.ts
@@ -1,4 +1,3 @@
-import type { PostAttributes } from "~/models/post.server";
 import type { Database, Tables } from "~/services/db.server";
 
 import { Post } from "~/models/post.server";
@@ -7,15 +6,10 @@ interface Services {
 	db: Database;
 }
 
-interface LikeAttributes extends PostAttributes {
-	readonly title: string;
-	readonly url: URL;
-}
-
 export class Like {
 	private post: Post;
 
-	constructor(post: Post | PostAttributes) {
+	constructor(post: Post) {
 		this.post = post instanceof Post ? post : new Post(post);
 	}
 
@@ -49,28 +43,29 @@ export class Like {
 	}
 
 	static async list({ db }: Services) {
-		let posts = await Post.list({ db }, "likes");
+		let posts = await Post.list({ db }, "like");
 		return posts.map((post) => new Like(post));
 	}
 
-	static async show({ db }: Services, id: Tables.Post["id"]) {
+	static async show({ db }: Services, id: Tables.SelectPost["id"]) {
 		let post = await Post.show({ db }, id);
 		return new Like(post);
 	}
 
 	static async create(
 		{ db }: Services,
-		input: Omit<LikeAttributes, "id" | "meta" | "typeId">,
+		input: Omit<Tables.InsertPost, "type"> & { title: string; url: URL },
 	) {
-		let post = await Post.create({ db }, "likes", {
-			...input,
-			meta: { title: input.title, url: input.url.toString() },
-		});
+		let post = await Post.create(
+			{ db },
+			{ ...input, type: "like" },
+			{ title: input.title, url: input.url.toString() },
+		);
 
 		return new Like(post);
 	}
 
-	static async destroy({ db }: Services, id: Tables.Post["id"]) {
+	static async destroy({ db }: Services, id: Tables.SelectPost["id"]) {
 		await Post.destroy({ db }, id);
 	}
 }
