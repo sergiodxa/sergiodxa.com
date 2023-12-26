c3a7e7d344db24a59f3dec20df2bbadd0137a102
Sergio Xalambrí <hello@sergiodxa.com>
2023-12-26 02:54:59 -0500

    Create /cms/articles route
    Make the loader auto import articles from Collected Notes

---
+++ app/routes/cms.articles/queries.tsx
@@ -0,0 +1,106 @@
+import type { AppLoadContext } from "@remix-run/cloudflare";
+import type { User } from "~/modules/session.server";
+
+import { eq } from "drizzle-orm";
+import fm from "front-matter";
+import { z } from "zod";
+
+import { Article } from "~/models/db-article.server";
+import { Markdown } from "~/modules/md.server";
+import { CollectedNotes } from "~/services/cn.server";
+import { Tables, database } from "~/services/db.server";
+
+const AttributesSchema = z
+	.object({
+		title: z.string(),
+		date: z.date(),
+		description: z.string(),
+		lang: z.string(),
+		tags: z.string(),
+		path: z.string(),
+		canonical_url: z.string().url(),
+		next: z.object({
+			title: z.string(),
+			path: z.string(),
+			description: z.string(),
+		}),
+		translate_from: z.object({
+			url: z.string().url(),
+			lang: z.string(),
+			title: z.string(),
+		}),
+		translated_to: z.object({ lang: z.string(), path: z.string() }).array(),
+	})
+	.partial();
+
+const FrontMatterSchema = z.object({
+	attributes: AttributesSchema,
+	body: z.string(),
+});
+
+export async function importArticles(context: AppLoadContext, user: User) {
+	let cn = new CollectedNotes(
+		context.env.CN_EMAIL,
+		context.env.CN_TOKEN,
+		context.env.CN_SITE,
+	);
+
+	let articles = await Promise.all([
+		cn.fetchNotes(1),
+		cn.fetchNotes(2),
+		cn.fetchNotes(3),
+		cn.fetchNotes(4),
+		cn.fetchNotes(5),
+	]).then((articles) => articles.flat());
+
+	let db = database(context.db);
+
+	await db.delete(Tables.posts).where(eq(Tables.posts.type, "article"));
+
+	await Promise.all(
+		articles.map(async (article) => {
+			let { body, attributes } = FrontMatterSchema.parse(fm(article.body));
+
+			body = stripTitle(body);
+
+			let plainBody = await Markdown.plain(body);
+
+			return await Article.create(
+				{ db },
+				{
+					title: attributes.title ?? article.title,
+					slug: attributes.path ?? article.path,
+					locale: attributes.lang ?? "en",
+					content: body,
+					excerpt: extractExcerpt({
+						body: plainBody.toString(),
+						headline: article.headline,
+						description: attributes.description,
+					}),
+					authorId: user.id,
+					createdAt: attributes.date ?? new Date(article.created_at),
+					updatedAt: new Date(article.updated_at),
+					canonical_url: attributes.canonical_url,
+				},
+			);
+		}),
+	);
+}
+
+function stripTitle(body: string) {
+	if (!body.startsWith("# ")) return body;
+	let [, ...rest] = body.split("\n");
+	return rest.join("\n").trim();
+}
+
+function extractExcerpt(input: {
+	body: string;
+	headline: string;
+	description?: string;
+}) {
+	if (input.description) return input.description;
+	if (!input.headline.includes("title: \n")) {
+		return `${input.headline.slice(-3)}…`;
+	}
+	return `${input.body.slice(0, 139)}…`.replaceAll("\n", " ");
+}
---
+++ app/routes/cms.articles/route.tsx
@@ -0,0 +1,77 @@
+import { redirect, type LoaderFunctionArgs, json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import {
+	Cell,
+	Column,
+	Row,
+	Table,
+	TableBody,
+	TableHeader,
+} from "react-aria-components";
+
+import { useT } from "~/helpers/use-i18n.hook";
+import { Article } from "~/models/db-article.server";
+import { SessionStorage } from "~/modules/session.server";
+import { database } from "~/services/db.server";
+
+import { importArticles } from "./queries";
+
+export async function loader({ request, context }: LoaderFunctionArgs) {
+	let user = await SessionStorage.requireUser(context, request, "/auth/login");
+	if (user.role !== "admin") throw redirect("/");
+
+	let db = database(context.db);
+
+	await importArticles(context, user);
+
+	let articles = await Article.list({ db });
+
+	return json({ articles: articles.map((article) => article.toJSON()) });
+}
+
+export default function Component() {
+	return (
+		<>
+			<header className="flex justify-between">
+				<h2 className="text-3xl font-bold">Articles</h2>
+			</header>
+
+			<ArticlesTable />
+		</>
+	);
+}
+
+function ArticlesTable() {
+	let { articles } = useLoaderData<typeof loader>();
+	let t = useT("translation", "cms.articles.table");
+
+	return (
+		<Table aria-label="Users" className="w-full">
+			<TableHeader>
+				<Column className="text-left" isRowHeader>
+					{t("header.title")}
+				</Column>
+				<Column className="text-right">{t("header.createdAt")}</Column>
+				<Column className="text-right">{t("header.updatedAt")}</Column>
+			</TableHeader>
+
+			<TableBody>
+				{articles.map((article) => {
+					return (
+						<Row key={article.id} className="py-2">
+							<Cell>
+								<a href={`/articles/${article.slug}`}>{article.title}</a>
+							</Cell>
+							<Cell className="flex-shrink-0 text-right">
+								{article.createdAt}
+							</Cell>
+							<Cell className="flex-shrink-0 text-right">
+								{article.updatedAt}
+							</Cell>
+						</Row>
+					);
+				})}
+			</TableBody>
+		</Table>
+	);
+}
