8ade66c4adc1a981e81205d3a5b9844346e63069
Sergio Xalambrí <hello@sergiodxa.com>
2023-12-26 17:22:45 -0500

    Update /articles queries to use new Cache module

--- app/routes/articles/queries.ts
+++ app/routes/articles/queries.ts
@@ -3,7 +3,7 @@ import type { AppLoadContext } from "@remix-run/cloudflare";
 import { z } from "zod";
 
 import { Article } from "~/models/db-article.server";
-import { Cache } from "~/services/cache.server";
+import { Cache } from "~/modules/cache.server";
 import { database } from "~/services/db.server";
 
 const SearchResultSchema = z.object({ path: z.string(), title: z.string() });
@@ -13,31 +13,26 @@ export async function queryArticles(
 	query: string | null,
 	noCache: boolean,
 ) {
-	let cache = new Cache(context.kv.cache);
+	let cache = new Cache.KVStore(context.kv.cache, context.waitUntil);
 	let db = database(context.db);
 
 	let key = query ? `articles:search:${query}` : "articles:list";
 
-	if (!noCache) {
-		let cached = await cache.get(key);
-		if (cached) return SearchResultSchema.array().parse(JSON.parse(cached));
-	}
+	let result = await cache.fetch(
+		key,
+		async () => {
+			let articles = query
+				? await Article.search({ db }, query)
+				: await Article.list({ db });
 
-	let articles = query
-		? await Article.search({ db }, query)
-		: await Article.list({ db });
-
-	let result = SearchResultSchema.array().parse(
-		articles.map((article) => {
-			return { path: article.pathname, title: article.title };
-		}),
+			return JSON.stringify(
+				articles.map((article) => {
+					return { path: article.pathname, title: article.title };
+				}),
+			);
+		},
+		{ ttl: 60 * 60 * 24 },
 	);
 
-	context.waitUntil(
-		cache.set(key, JSON.stringify(result), {
-			expirationTtl: 60 * 60 * 24,
-		}),
-	);
-
-	return result;
+	return SearchResultSchema.array().parse(JSON.parse(result));
 }
