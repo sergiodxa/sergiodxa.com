745d9211ba90973adf9e5bd02ff62298ab2539f1
Sergio Xalambrí <hello@sergiodxa.com>
2023-12-26 02:51:40 -0500

    Update Markdown module to parse to plain text

--- app/modules/md.server.ts
+++ app/modules/md.server.ts
@@ -1,7 +1,13 @@
 import { parse, transform, type Config } from "@markdoc/markdoc";
+import { remark } from "remark";
+import strip from "strip-markdown";
 
 export class Markdown {
 	static parse(content: string, options: Config = {}) {
 		return transform(parse(content), options);
+	}
+
+	static plain(text: string) {
+		return remark().use(strip).process(text);
 	}
 }
