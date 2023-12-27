12eb4c8e15381714ba40c9611e00f3774b65acf1
Sergio Xalambrí <hello@sergiodxa.com>
2023-12-27 16:31:08 -0500

    Fix mobile Edit Article button on /cms/articles

--- app/routes/cms.articles/article-list.tsx
+++ app/routes/cms.articles/article-list.tsx
@@ -45,7 +45,7 @@ function Item(props: ItemProps) {
 				</div>
 			</div>
 
-			<div>
+			<div className="flex-shrink-0">
 				<Link
 					to={props.id}
 					className="block rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 no-underline shadow-sm ring-1 ring-inset ring-gray-300 visited:text-gray-900 hover:bg-gray-50"
