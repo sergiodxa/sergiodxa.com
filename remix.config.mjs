9280ce5bcc51f5619e7a11e1f67f1f022e7dada8
Sergio Xalambrí <hello@sergiodxa.com>
2024-01-16 12:29:49 -0500

    Enable v3 future flags

--- remix.config.mjs
+++ remix.config.mjs
@@ -13,6 +13,8 @@ export default {
 	tailwind: true,
 	watchPaths: ["./server/**/*.ts"],
 	future: {
+		v3_relativeSplatPath: true,
+		v3_fetcherPersist: true,
 	},
-	serverNodeBuiltinsPolyfill: { modules: {}, },
+	serverNodeBuiltinsPolyfill: { modules: {} },
 };
