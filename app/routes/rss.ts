b47b3ddef7b23db1cd45cd7ad2237a4f430ddd39
Sergio Xalambrí <hello@sergiodxa.com>
2024-06-16 01:49:38 -0500

    Add glossary terms to RSS feed

--- app/routes/rss.ts
+++ app/routes/rss.ts
@@ -3,6 +3,7 @@ import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { xml } from "remix-utils/responses";
 
 import { Article } from "~/models/article.server";
+import { Glossary } from "~/models/glossary.server";
 import { Like } from "~/models/like.server";
 import { Tutorial } from "~/models/tutorial.server";
 import { Logger } from "~/modules/logger.server";
@@ -13,20 +14,25 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
 	void new Logger(context).http(request);
 
 	let db = database(context.db);
-	let [articles, likes, tutorials] = await Promise.all([
+	let [articles, likes, tutorials, glossary] = await Promise.all([
 		Article.list({ db }),
 		Like.list({ db }),
 		Tutorial.list({ db }),
+		Glossary.list({ db }),
 	]);
 
 	let feed = [
 		...articles.map((article) => {
 			let link = new URL(article.pathname, "https://sergiodxa.com").toString();
 
+			let description = article.excerpt
+				? `${article.excerpt}\n<a href="${link}">Read it on the web</a>`
+				: `<a href="${link}">Read it on the web</a>`;
+
 			return {
 				guid: article.id,
 				title: article.title,
-				description: `${article.excerpt}\n<a href="${link}">Read it on the web</a>`,
+				description,
 				link,
 				pubDate: article.createdAt.toUTCString(),
 			};
@@ -41,12 +47,33 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
 		...tutorials.map((tutorial) => {
 			let link = new URL(tutorial.pathname, "https://sergiodxa.com").toString();
 
+			let description = tutorial.excerpt
+				? `${tutorial.excerpt}\n<a href="${link}">Read it on the web</a>`
+				: `<a href="${link}">Read it on the web</a>`;
+
 			return {
 				guid: tutorial.id,
 				title: tutorial.title,
-				description: `${tutorial.excerpt}\n<a href="${link}">Read it on the web</a>`,
+				description,
 				link,
 				pubDate: tutorial.createdAt.toUTCString(),
+			};
+		}),
+		...glossary.map((glossaryTerm) => {
+			let url = new URL("https://sergiodxa.com/glossary");
+			url.hash = glossaryTerm.slug;
+
+			let link = url.toString();
+			let title = glossaryTerm.title
+				? `${glossaryTerm.term} (aka ${glossaryTerm.title})`
+				: glossaryTerm.term;
+
+			return {
+				guid: glossaryTerm.id,
+				title,
+				description: glossaryTerm.definition,
+				link,
+				pubDate: glossaryTerm.createdAt.toUTCString(),
 			};
 		}),
 	].sort(
@@ -56,7 +83,7 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
 	let rss = new RSS({
 		title: "Sergio Xalambrí",
 		description:
-			"The complete list of articles, bookmarks and tutorials of @sergiodxa.",
+			"The complete list of articles, bookmarks, tutorials, and glossary terms of @sergiodxa.",
 		link: "https://sergiodxa.com/rss",
 	});
 
