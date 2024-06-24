90cd97e12f136feb05e12c90c4e2bbe198de6f98
Sergio Xalambrí <hello@sergiodxa.com>
2024-06-24 18:48:42 -0500

    Add ULID to /id

--- app/routes/id.tsx
+++ app/routes/id.tsx
@@ -2,49 +2,39 @@ import { createId } from "@paralleldrive/cuid2";
 import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 import { useId } from "react";
+import { ulid } from "ulidx";
 
 export function loader() {
 	return json({
 		uuid: crypto.randomUUID(),
 		cuid: createId(),
+		ulid: ulid(),
 	});
 }
 
 export default function Component() {
-	let { uuid, cuid } = useLoaderData<typeof loader>();
+	let { uuid, cuid, ulid } = useLoaderData<typeof loader>();
 	let id = useId();
 
 	return (
 		<main className="flex min-h-screen w-full flex-col items-center justify-center gap-6 font-mono">
-			<div className="flex w-full max-w-screen-sm flex-col gap-2">
-				<label
-					htmlFor={`uuid-${id}`}
-					className="text-xl font-bold tracking-wide"
-				>
-					UUID
-				</label>
-				<output
-					id={`uuid-${id}`}
-					className="select-all bg-zinc-200 p-4 dark:bg-zinc-800"
-				>
-					{uuid}
-				</output>
-			</div>
-
-			<div className="flex w-full max-w-screen-sm flex-col gap-2">
-				<label
-					htmlFor={`cuid-${id}`}
-					className="text-xl font-bold tracking-wide"
-				>
-					CUID
-				</label>
-				<output
-					id={`cuid-${id}`}
-					className="select-all bg-zinc-200 p-4 dark:bg-zinc-800"
-				>
-					{cuid}
-				</output>
-			</div>
+			<Identifier label="UUID" value={uuid} />
+			<Identifier label="CUID" value={cuid} />
+			<Identifier label="ULID" value={ulid} />
 		</main>
+	);
+}
+
+function Identifier({ label, value }: { label: string; value: string }) {
+	let id = useId();
+	return (
+		<div className="flex w-full max-w-screen-sm flex-col gap-2">
+			<label htmlFor={id} className="text-xl font-bold tracking-wide">
+				{label}
+			</label>
+			<output id={id} className="select-all bg-zinc-200 p-4 dark:bg-zinc-800">
+				{value}
+			</output>
+		</div>
 	);
 }
