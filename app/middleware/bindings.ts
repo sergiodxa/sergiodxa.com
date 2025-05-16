5bff8c64b1461c8dcc4dfcce27d4664b41efe7f5
Sergio Xalambrí <hello@sergiodxa.com>
2025-05-16 04:04:54 -0500

    Remove GITHUB_CONTENT_REPO from environment bindings

--- app/middleware/bindings.ts
+++ app/middleware/bindings.ts
@@ -28,7 +28,6 @@ export function getBindings() {
 				GITHUB_CLIENT_ID: z.string().min(1),
 				GITHUB_CLIENT_SECRET: z.string().min(1),
 				GITHUB_CALLBACK_URL: z.string().url(),
-				GITHUB_CONTENT_REPO: z.string().min(1),
 				GITHUB_TOKEN: z.string().min(1),
 				GITHUB_USERNAME: z.string().min(1),
 
