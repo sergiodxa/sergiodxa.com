3826413935256a5a871c516883c7f5e71c38c36d
Sergio Xalambrí <hello@sergiodxa.com>
2025-05-16 04:07:04 -0500

    Remove unused environment variables from .env.example and bindings.ts

--- app/middleware/bindings.ts
+++ app/middleware/bindings.ts
@@ -21,15 +21,11 @@ export function getBindings() {
 		waitUntil: ctx.waitUntil.bind(ctx),
 		env: z
 			.object({
-				BASE_URL: z.string().min(1).url(),
-
 				COOKIE_SESSION_SECRET: z.string().min(1),
 
 				GITHUB_CLIENT_ID: z.string().min(1),
 				GITHUB_CLIENT_SECRET: z.string().min(1),
 				GITHUB_CALLBACK_URL: z.string().url(),
-				GITHUB_TOKEN: z.string().min(1),
-				GITHUB_USERNAME: z.string().min(1),
 
 				GH_APP_ID: z.string().min(1),
 				GH_APP_PEM: z.string().min(1),
