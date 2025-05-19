ed81670af463b097ae51af16beb1c3f7d6694dbe
Sergio Xalambrí <hello@sergiodxa.com>
2025-05-19 12:40:04 -0500

    Refactor context initialization in entry.worker.ts to use unstable_InitialContext type

--- app/entry.worker.ts
+++ app/entry.worker.ts
@@ -1,5 +1,6 @@
 import * as build from "virtual:react-router/server-build";
 import type { ExecutionContext } from "@cloudflare/workers-types";
+import type { unstable_InitialContext } from "react-router";
 import { createRequestHandler } from "react-router";
 import { CloudflareContext } from "./middleware/bindings";
 
@@ -7,7 +8,8 @@ const handler = createRequestHandler(build);
 
 export default {
 	async fetch(request: Request, env: Cloudflare.Env, ctx: ExecutionContext) {
-		let context = new Map([[CloudflareContext, { env, ctx, cf: request.cf }]]);
+		let context: unstable_InitialContext = new Map();
+		context.set(CloudflareContext, { env, ctx, cf: request.cf });
 		return await handler(request, context);
 	},
 };
